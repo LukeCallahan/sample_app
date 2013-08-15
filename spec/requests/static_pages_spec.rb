aac3e60670af9585668c15c7f66cfea30ac0eb07
Luke <lukemcal@gmail.com>
2013-08-15 16:38:57 -0700

    finishing the exercises from Chapter 5

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -9,11 +9,18 @@ describe "Static Pages" do
 
 	subject { page }
 
+	shared_examples_for "all static pages" do
+		it { should have_selector('h1', text: heading) }
+		it { should have_title(full_title(page_title)) }
+	end
+
 	describe "Home page" do
 		before { visit root_path }
 
-		it { should have_content('Sample_App') }
-		it { should have_title(full_title('')) }
+		let(:heading) { 'SampleApp' }
+		let(:page_title) { '' }
+
+		it_should_behave_like "all static pages"
 		it { should_not have_title('| Home') }
 
 	end
@@ -22,7 +29,7 @@ describe "Static Pages" do
 		before { visit about_path }
 
 		it { should have_content('About') }
-		it { should have_title(full_title('About Us')) }
+		#it { should have_title(full_title('About Us')) }
 	
 	end
 
@@ -31,13 +38,30 @@ describe "Static Pages" do
 		before { visit help_path }
 
 		it { should have_content('Help') }
-		it { should have_title(full_title('Help')) }
+		#it { should have_title(full_title('Help')) }
 
 	end
 
 	describe "Contact page" do
-		it { should have_content('Contact') }
-		it { should have_title(full_title('Contact')) }
+		before { visit contact_path }
 
+		it { should have_selector('h1', text: 'Contact') }
+		#it { should have_title(full_title('Contact')) }
+
+	end
+
+	it "should have the right links on they layout" do
+		visit root_path
+		click_link "About"
+		expect(page).to_have_title(full_title('About Us'))
+		click_link "Help"
+		expect(page).to_have_title(full_title('Help'))
+		click_link "Contact"
+		expect(page).to_have_title(full_title('Contact'))
+		click_link "Home"
+		click_link "Sign up now!"
+		expect(page).to_have_title(full_title('Sign up'))
+		click_link "sample app"
+		expect(page).to_have_title(full_title('sample app'))
 	end
 end
